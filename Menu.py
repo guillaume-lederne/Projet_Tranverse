51e4c3dec96e1dfefe1cd119080342c635d83b43
guillaume-lederne <guillaume.lederne@efrei.net>
2025-05-19 10:51:54 +0200

    Création fenêtre option

--- Menu.py
+++ Menu.py
@@ -1,18 +1,21 @@
 import tkinter as tk
 from PIL import Image, ImageTk
+from Options import affichage_icone
 
 def lancer_jeu():
     print("Lancement du jeu...")
 
 def ouvrir_options():
     print("Ouverture des options...")
+    affichage_icone()
+
 
 root = tk.Tk()
 root.title("Menu du Jeu")
 root.geometry("1184x672")
 root.resizable(False, False)
 
-image_path = "Image_Menu.jpeg"
+image_path = "image/Image_Menu.jpeg"
 try:
     image = Image.open(image_path)
     photo = ImageTk.PhotoImage(image)
@@ -26,7 +29,7 @@ background.image = photo
 background.place(x=0, y=0, relwidth=1, relheight=1)
 
 
-btn_jouer_image_path = "jouer_transparent.png"  # Assurez-vous que cette image existe
+btn_jouer_image_path = "image/jouer_transparent.png"  # Assurez-vous que cette image existe
 try:
     btn_jouer_image = Image.open(btn_jouer_image_path)
     btn_jouer_photo = ImageTk.PhotoImage(btn_jouer_image)
@@ -39,7 +42,7 @@ btn_jouer = tk.Button(root, image=btn_jouer_photo, command=lancer_jeu, borderwid
 btn_jouer.image = btn_jouer_photo
 btn_jouer.place(x=510, y=287, width=160, height=50)
 
-btn_option_image_path = "option_transparent.png"
+btn_option_image_path = "image/option_transparent.png"
 try:
     btn_option_image = Image.open(btn_option_image_path)
     btn_option_photo = ImageTk.PhotoImage(btn_option_image)
