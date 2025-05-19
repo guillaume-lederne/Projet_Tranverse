ba9271521785a6e86abf9972e96e766a8e4e303f
manon <manon.souchon--garrigue@efrei.net>
2025-05-19 23:49:33 +0200

    reparation deuxième tank

--- terrain_file.py
+++ terrain_file.py
@@ -31,8 +31,7 @@ class Terrain:
             return self.hauteur_terrain + 40 * m.sin(x * 0.005),40*0.005*m.cos(x * 0.005)#+image du terrain DESSINEE PAR CLEMENT
         def f2(x):
             return self.hauteur_terrain + 50*m.cos(x/90)+44*m.sin(100)+50*m.sin(x/63),-(5/9)*m.sin(x/90)+44/100*m.cos(x/100)+50/63*m.cos(x/63)#+image du terrain DESSINEE PAR CLEMENT
-        # def f3(x):
-        #     return self.hauteur_terrain + 300*m.cos(x/30),-10*m.sin(x/30)
+
         def f4(x):
             return self.hauteur_terrain + 50*m.sin(x/49)+42*m.cos(x/42-400),50/49*m.cos(x/49)-m.sin(x/49)
         def f5(x):
